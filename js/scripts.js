c29c0a78f916c38888034c559a9b72d919b03b1f
Student <example@example.com>
2019-08-30 14:45:11 -0700

    Rewrite variables name with camelhump style.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,43 @@
-function Pizza(pizzatop, pizzasize) {
-  this.pizzasize = pizzasize,
-  this.pizzaprice = 0,
- this.pizzatop = pizzatop
-}
-
-
-   Pizza.prototype.calprice = function() {
-   if(this.pizzasize === "6" && this.pizzatop === "1") {
-    this.pizzaprice = 4;
-    }
+// Business Logic for Pizza -------
+function Pizza(pizzasize, pizzatop,) {
+  this.pizzaSize = pizzasize,
+  this.pizzaTop = pizzatop,
+  this.pizzaPrice = 90
 }
 
 
 
+Pizza.prototype.calPrice = function() {
+  if(this.pizzaSize === "6" && this.pizzaTop === "1") {
+    this.pizzaPrice = 4;
+  } else if(this.pizzaSize === "6" && this.pizzaTop === "2") {
+    this.pizzaPrice = 6;
+  } else if(this.pizzaSize === "6" &&  this.pizzaTop === "3"){
+    this.pizzaPrice = 8;
+  } else if(this.pizzaSize === "6" &&  this.pizzaTop === "4"){
+    this.pizzaPrice = 10;
+    console.log(pizzaPrice);
+  }
+}
+
+
+//user interface logic-----
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-  var checkedpizzatop = $(":checkbox:checked").length;
-  var checkedpizzasize = parseInt($("input:radio[name=size]:checked").val());
-    console.log(checkedpizzasize);
-    console.log(checkedpizzatop);
-      var newPizza = new Pizza(checkedpizzatop, checkedpizzasize);
-      newPizza.calprice();
-      console.log(newPizza.pizzaprice);
+    var checkedPizzaTop = $(":checkbox:checked").length;
+    var checkedPizzaSize = parseInt($("input:radio[name=size]:checked").val());
+
+    console.log(checkedPizzaSize);
+    console.log(checkedPizzaTop);
+    var newPizza = new Pizza(checkedPizzaTop, checkedPizzaSize);
+    newPizza.calPrice();
+    console.log(newPizza.pizzaPrice);
 
 
-      var finalPrice = newPizza.pizzaprice;
-          $(".finalprice").text(finalPrice);
-          $("#result-price").show();
+    var finalPrice = newPizza.pizzaPrice;
+    $(".finalprice").text(finalPrice);
+    $("#result-price").show();
 
 
   })
